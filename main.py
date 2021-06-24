cd0f63264f5bda4db94e621217b049c6a88cbb8f
Gimba <martin.rosellen@fu-berlin.de>
2021-06-24 09:03:45 +0200

    main method merely for testing purposes

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+import image_helpers as ih
+import os
+from PIL import Image
+
 
 def main():
-    exit()
+    folder = "test_data"
+    files = os.listdir(folder)
+    files = [folder + "/" + f for f in files]
+    for file in files:
+
+        if ih.is_image(file):
+            img = Image.open(file).convert('RGB')
+            print(ih.is_grey_scale(img))
+
 
 if __name__ == '__main__':
     main()

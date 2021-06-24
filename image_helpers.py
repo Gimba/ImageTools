1a84e100ed3a5be20996e4fe924640cf8eaab634
Gimba <martin.rosellen@fu-berlin.de>
2021-06-24 08:51:47 +0200

    changed input type of function to PIL.Image and addded comments

--- image_helpers.py
+++ image_helpers.py
@@ -1,16 +1,21 @@
 from PIL import Image
 
-def is_grey_scale(file):
-    img = Image.open(file).convert('RGB')
+
+def is_grey_scale(img: Image) -> bool:
+    """
+    Used to separate day and night pictures
+
+    :String file:
+    :return: True if image and is greyscale
+    """
     w, h = img.size
     diff = 0
-    for i in [0, w//2, w-1]:
-        for j in [0, h//2, h-1]:
-            r, g, b = img.getpixel((i,j))
+    for i in [0, w // 2, w - 1]:
+        for j in [0, h // 2, h - 1]:
+            r, g, b = img.getpixel((i, j))
             rg = abs(r - g)
             rb = abs(r - b)
             gb = abs(g - b)
             diff += rg + rb + gb;
 
     return diff < 100
-
